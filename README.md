1f450305ec17628e606923384a0bb6ca7a11a5b2
plasmashadow <plasmashadowx@gmail.com>
2016-01-26 00:47:12 +0530

    changes to README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ##WebRTC Monitor
+
+Chrome Extension used to monitor WebRTC traffic. Currently only support
+Video Traffic.
+
+![](https://raw.githubusercontent.com/plasmashadow/webrtc-monitor/master/shreenshot.png)
+
+##Instruction
+
+Currently Works on sites which uses adapter.js.
+
+##RoadMap
+
+* adding audio monitor.
+* adding patching for chrome and firefox native RTCPeerConnection.
+
+##license
+BSD
--- app/background.js
+++ app/background.js
@@ -2,7 +2,6 @@ function StatReciver(callback){
   var prevVidByteSend = 0;
   var prevAudByteSend = 0;
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-     console.log(message)
      var request = message.message;
       if(message.for == 'stat'){
         if(prevVidByteSend == 0)
@@ -24,31 +23,36 @@ function StatReciver(callback){
       }
   });
 }
-
+/*works only for same dimentional arrays*/
+function zip(x,y){
+  return x.map(function(element, index){
+    return [element, y[index]];
+  })
+}
 
 $(document).ready(function(){
   var bitrate = document.getElementById("bitrate");
-  var datset = [[0,0],[1,0],[2,0],[3,0],[4,0]]
-  var count = 5;
-  var plot = $.plot("#bitrate", [datset], {
+  var x_axis = [0,1,2,3,4];
+  var y_axis = [0,0,0,0,0];
+  var count = 0;
+  var plot = $.plot("#bitrate",[zip(x_axis,y_axis)], {
 			series: {
 				shadowSize: 0	// Drawing is faster without shadows
 			},
 			yaxis: {
 				min: 0,
-				max: 100
+				max: 1000
 			},
 			xaxis: {
 				show: false
 			}
 		});
-  plot.draw();
   StatReciver(function(stat){
      var videoBitRate  = Math.abs(stat.video.bandWidth)/(1024*8);
-     datset.push([count++, videoBitRate])
-     datset.shift();
-     plot.setData(datset);
-     console.log(datset);
+     y_axis.shift();
+     y_axis.push(videoBitRate)
+     console.log(y_axis)
+     plot.setData([zip(x_axis,y_axis)]);
      plot.draw();
   });
 })
